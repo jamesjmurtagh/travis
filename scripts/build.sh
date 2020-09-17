cfa55f193ec1a8143a97059c1d5cca75dcc12f37
James Murtagh <jamesjmurtagh@outlook.com>
2020-09-17 13:48:15 +1000

    fix script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o windows-vorteil.exe
-CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o linux-vorteil
-CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o darwin-vorteil
+CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o vorteil-windows.exe
+CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o vorteil-linux
+CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o vorteil-darwin
