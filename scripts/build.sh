b0bc4603fd9326a591ef781c4bb0c5dc8c76bb2e
James Murtagh <jamesjmurtagh@outlook.com>
2020-09-17 13:39:16 +1000

    Testing multi-file release

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,5 @@
-go build -o helloworld main.go
+#!/bin/bash
+mkdir -p windows && mkdir -p linux && mkdir -p darwin
+CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o windows/vorteil.exe
+CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o linux/vorteil
+CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o darwin/vorteil
