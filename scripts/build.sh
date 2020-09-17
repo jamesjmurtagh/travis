ba0ee3f0f8cc283e0c8198630c29f6b1fa9a7913
James Murtagh <jamesjmurtagh@outlook.com>
2020-09-18 07:38:44 +1000

    Try new approach to release files; also messing with a second travis file.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o vorteil-windows.exe
-CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o vorteil-linux
-CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o vorteil-darwin
+CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o vorteil.exe
+zip vorteil_windows-x86.zip vorteil.exe && rm vorteil.exe
+CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o vorteil
+tar -zcvf vorteil_linux-x86.tar.gz vorteil && rm vorteil
+CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o vorteil
+tar -zcvf vorteil_darwin-x86.tar.gz vorteil && rm vorteil
