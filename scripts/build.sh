dc1227dbf26a7675f303b32277aa83104ba0ac0b
James Murtagh <jamesjmurtagh@outlook.com>
2020-09-18 07:57:00 +1000

    Test multi OS

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o vorteil.exe
-zip vorteil_windows-x86.zip vorteil.exe && rm vorteil.exe
-CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o vorteil
-tar -zcvf vorteil_linux-x86.tar.gz vorteil && rm vorteil
-CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o vorteil
-tar -zcvf vorteil_darwin-x86.tar.gz vorteil && rm vorteil
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+
+    CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o vorteil
+    tar -zcvf vorteil_darwin-x86.tar.gz vorteil && rm vorteil
+
+else
+
+    CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o vorteil.exe
+    zip vorteil_windows-x86.zip vorteil.exe && rm vorteil.exe
+    CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o vorteil
+    tar -zcvf vorteil_linux-x86.tar.gz vorteil && rm vorteil
+
+fi
