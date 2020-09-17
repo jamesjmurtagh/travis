105e7f3b6c9c64da838d0b840bb66d66ab4d0e95
James Murtagh <jamesjmurtagh@outlook.com>
2020-09-17 13:43:37 +1000

    Test #2 for multifile releases

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir -p windows && mkdir -p linux && mkdir -p darwin
-CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o windows/vorteil.exe
-CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o linux/vorteil
-CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o darwin/vorteil
+CGO_ENABLED=0 GOOS=windows go build -a -tags netgo -ldflags '-w' -o windows-vorteil.exe
+CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' -o linux-vorteil
+CGO_ENABLED=0 GOOS=darwin go build -a -tags netgo -ldflags '-w' -o darwin-vorteil
